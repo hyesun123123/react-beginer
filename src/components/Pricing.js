00c16b41827f3d07f0348261f73c829a7ede2975
hyesun123123 <goodnight4857@naver.com>
2020-11-03 12:08:56 +0900

    add_Footer

---
+++ src/components/Pricing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Pricing() {
+    return (
+        <div>
+            <div className="pricing__section">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Pricing
