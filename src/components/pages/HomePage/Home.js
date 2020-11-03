dc38607f564ce87dda3426e1d8cf3c07929591a7
hyesun123123 <goodnight4857@naver.com>
2020-11-03 11:38:09 +0900

    first-commit

---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import {homeObjOne, homeObjThree, homeObjTwo, homeObjFour} from './Data'
+function Home() {
+    return (
+        <>
+            <HeroSection {...homeObjOne}/>
+            <HeroSection {...homeObjThree}/>
+            <HeroSection {...homeObjTwo}/>
+            <HeroSection {...homeObjFour}/>
+        </>
+    )
+}
+
+export default Home
